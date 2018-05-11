7508c1c8691f7ccb517a1d59acfa6112864b4421
Shawon Ashraf <shawon13@live.com>
2018-05-11 17:15:01 +0600

    fixed pod dep

--- RT Tracker iOS/ViewController.swift
+++ RT Tracker iOS/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import PusherSwift
+import Alamofire
+import GoogleMaps
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var simulateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func simulateLocation(_ sender: Any) {
+    }
+    
 }
 
